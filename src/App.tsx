f8cc1d10ef2599b2115035df56cbc2680a9efe5f
parham sankian <p.sankian2004@gmail.com>
2025-02-28 09:26:57 -0800

    retro background and safari mockup

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,31 @@
 
 //import Musiccard from './comps/music-card'
 //import Searchbar from './comps/search-bar'
+//import Stars from './comps/stars'
+import { RetroGrid } from './components/magicui/retro-grid'
+import { Safari } from './components/magicui/safari'
+
+
 import './App.css'
 
 function App() {
 
 
   return (
-   <div> hello world
-    
-    
-   </div>
+    <div >'
+     <div >
+      {/* Background Grid */} 
+      <RetroGrid className="absolute inset-0 -z-10  " angle={50} lightLineColor='purple' opacity={1} />
+
+      {/* Main Content */}
+      <div className="flex justify-center">
+      <Safari url="parham-music-player" className="size-8/12 dark " />
+     
+    </div>
+     
+      
+    </div>
+  </div>
   )
 }
 
